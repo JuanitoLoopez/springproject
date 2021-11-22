a549f47533d449929392deeecdc1a32cdbb63649
JuanitoLopez <66218285+JuanitoLoopez@users.noreply.github.com>
2021-11-22 08:57:19 -0600

    Add files via upload

---
+++ springproyectdemo/src/main/java/com/springproyectdemo/springproyectdemo/SpringproyectdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springproyectdemo.springproyectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringproyectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringproyectdemoApplication.class, args);
+	}
+
+}
---
+++ springproyectdemo/src/test/java/com/springproyectdemo/springproyectdemo/SpringproyectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproyectdemo.springproyectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringproyectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
